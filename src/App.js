f5e6d06a3c85a96a08f892aa5ec4b8c8717763f9
Nick Ciambrone <nickciambrone@Nicks-MBP.fios-router.home>
2021-12-29 23:24:07 -0500

    t

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/header/header.component'
+import React from 'react';
+import Home from './components/home/home.component'
+import Services from './components/services/services.component';
+import About from './components/about/about.component';
+import Contact from './components/contact/contact.component';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      activePage : 'home'
+    }
+  }
+  handleClick = (page)=>{
+    this.setState({activePage:page})
+  }
+  render(){
+    console.log(this.state.activePage)
+  return (
+    <div className="App">
+     <Header activePage = {this.state.activePage} handleClick = {this.handleClick}/>
+     {
+       this.state.activePage==='home' ?
+       <Home /> :
+       this.state.activePage==='services' ?
+       <Services /> :
+       this.state.activePage==='about' ?
+       <About /> :
+       this.state.activePage==='contact' ?
+       <Contact /> :
+       <Home />
+
+     }
+    </div>
+  );
+}
+}
+
+export default App;
